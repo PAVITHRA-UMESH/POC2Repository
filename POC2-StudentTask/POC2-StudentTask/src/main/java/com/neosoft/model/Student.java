6570c3317963b7905092290d6e26b23d2c42edf2
PAVITHRA-UMESH <90679183+PAVITHRA-UMESH@users.noreply.github.com>
2022-01-19 19:22:26 +0530

    Added POC2 files- Initial commit.

---
+++ POC2-StudentTask/POC2-StudentTask/src/main/java/com/neosoft/model/Student.java
@@ -0,0 +1,102 @@
+package com.neosoft.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "student")
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotNull
+	@Size(max = 65)
+	private String firstName;
+
+	@NotNull
+	@Size(max = 65)
+	private String lastName;
+
+	@NotNull
+	@Size(max = 10)
+	private String mobile;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Project getProject() {
+		return project;
+	}
+
+	public void setProject(Project project) {
+		this.project = project;
+	}
+
+	@NotNull
+	@Size(max = 50)
+	private String email;
+
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "projid", nullable = false)
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	private Project project;
+
+}
