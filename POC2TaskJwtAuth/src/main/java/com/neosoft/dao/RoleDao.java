4afa9037d8816facd087d47225e06831ef1409ae
PAVITHRA-UMESH <90679183+PAVITHRA-UMESH@users.noreply.github.com>
2022-01-19 21:48:30 +0530

    Adding JWT.

---
+++ POC2TaskJwtAuth/src/main/java/com/neosoft/dao/RoleDao.java
@@ -0,0 +1,9 @@
+package com.neosoft.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.neosoft.entity.Role;
+
+public interface RoleDao extends JpaRepository<Role, String> {
+
+}
